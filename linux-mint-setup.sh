ee63d004afc2a0b8fa3a6432901be62fe522bde2
Edvinas <evinas108@gmail.com>
2017-11-21 20:37:11 +0200

    Add more commands

--- linux-mint-setup.sh
+++ linux-mint-setup.sh
@@ -36,7 +36,13 @@ sudo apt-get install -y dropbox
 #
 # Directories, look and feel.
 #
+
+# Common project dir.
 mkdir ~/Documents/Projects
+
+# Lock screen shortcut.
+gsettings set org.cinnamon.desktop.keybindings.media-keys screensaver \
+    "['<Super>l', 'XF86ScreenSaver']"
 
 
 #
@@ -49,11 +55,11 @@ git config --global user.name 'Edvinas'
 git config --global user.email 'evinas108@gmail.com'
 
 # Hack to enable silent Java installation.
-echo debconf shared/accepted-oracle-license-v1-1 select true |\
-sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+    sudo debconf-set-selections
 
-echo debconf shared/accepted-oracle-license-v1-1 seen true |\
-sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+    sudo debconf-set-selections
 
 # Install Java 8 and set it as default.
 sudo add-apt-repository -y ppa:webupd8team/java
@@ -61,13 +67,41 @@ sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y install oracle-java8-set-default
 
-echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle/jre/bin/java"' |\
-sudo tee -a /etc/environment
+echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle/jre/bin/java"' | \
+    sudo tee -a /etc/environment
+
+# Setup best IDE for Java (automatically grep latest version).
+ideaVersion=$(
+    curl https://www.jetbrains.com/updates/updates.xml |\
+    grep 'IDEA_Release' -1 |\
+    grep -oP 'version="\K[^"]+'
+)
+
+wget -O idea.tar.gz \
+    "https://download.jetbrains.com/idea/ideaIU-$ideaVersion.tar.gz"
+
+sudo mkdir /opt/idea
+sudo tar xf idea.tar.gz --strip 1 -C /opt/idea
+rm idea.tar.gz
+
+sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
 
 # VirtualBox for setting up virtual machines.
 sudo apt-get install -y virtualbox
 mkdir ~/Documents/VMs
 vboxmanage setproperty machinefolder ~/Documents/VMs
 
+# Setup chefdk.
+url=https://packages.chef.io/files/stable/chefdk
+url+=/2.3.4/debian/8/chefdk_2.3.4-1_amd64.deb
+wget $url
+
+sudo dpkg -i chefdk_2.3.4-1_amd64.deb
+rm chefdk_2.3.4-1_amd64.deb
+
 # Useful VirtualBox setup tool.
-sudo apt-get install -y vagrant
+wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb
+sudo dpkg -i vagrant_2.0.1_x86_64.deb
+rm vagrant_2.0.1_x86_64.deb
+
+vagrant plugin install vagrant-berkshelf
