2b04f9fb39813f251e7263fc92b87fec6a10460c
Edvinas <edvinas108@gmail.com>
2017-11-24 15:55:12 +0200

    Add node

--- linux-mint-setup.sh
+++ linux-mint-setup.sh
@@ -175,3 +175,7 @@ rm postman.tar.gz
 
 # Packet sniffing / viewing tool, install silently.
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install wireshark
+
+# JavaScript development tools.
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
